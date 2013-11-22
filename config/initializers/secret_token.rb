afd788f05cc31310c730aa0f270e83a50544e3f8
rick_barkley <rick_barkley@nuvola-networks.com>
2013-11-22 00:40:57 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BarkleyAdoption::Application.config.secret_token = 'ce8a8b3292478f6b42c1cfb8dd3defc88279c957d43cb85ab9b28cd414727def06dec2d4f346d63f26a0e984bea0d2a33510bcf77fbe8c785bfbc28c0338308a'
