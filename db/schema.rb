5444c7d47ea5aefa5d3f02c4bbbe6caadffc48e1
rick_barkley <rick_barkley@nuvola-networks.com>
2014-05-25 13:56:43 -0500

    updated devise views and added name field

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140525180222) do
+ActiveRecord::Schema.define(:version => 20140525183959) do
 
   create_table "ckeditor_assets", :force => true do |t|
     t.string   "data_file_name",                  :null => false
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(:version => 20140525180222) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.string   "name"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
