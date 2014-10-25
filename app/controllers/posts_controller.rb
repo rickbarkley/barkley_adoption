3a117858bc01bca1197c00315db386f5da1a4c4f
Rick Barkley <rick_barkley@nuvola-networks.com>
2014-10-25 00:40:16 -0500

    made small changes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all
+    @posts = Post.all.order( 'post.id DESC' )
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @posts }
